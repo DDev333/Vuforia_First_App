c81312090ef1045a2d3553ed9c33f5bd649ed744
Devesh Ghodpage <deveshghodpage333@yahoo.in>
2025-09-16 20:30:26 +0530

    Attached an object to marker

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWRyOWQwai8vLy8vQUFBQm1UY25XMlRhZ0U4N3VmSnF4dWZSUktLSDBCZXF4cjhyMitPRGI4NnBvNTF3ZmJwd0hLbkd5bHRzZlkvYlpmaU5jM25adklZWnlTUDNLUG9vQTRiLzRsdTFrNXRCNWJDVEl0R25IOHJGK0VZRjF0eDEzREdyNVJYUC91cko4VGdkSXJLbWFCL2RNaFRsM2o4K1VUN1N6dHQ4S2VLNCt2aXE5MFdaRS9jbzRpbzhVblFQaktraFgyWkdtdThRMUdORXdwaS9XL3diK0dQR0hVTGY0eTk0ZEd5VENCNmVmbVpvb0xRaHdNVVpSNFlzS2o5Wk9tbEZwRFptZUpWQUxkRHZoZkJSVXRkaXhmLzh1WlVZRGVMN1IyY3cyNGZVU1B1eDNLdXhGOVJFVFB4UFcxRGhJY3JjUnBtc0E5MjlablBEb1pWeFNMSENWVU1EeFcxQ004RnQ3eTdoQWtlQ0tHRTRmWmRiYVRIQWtVSFE=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
